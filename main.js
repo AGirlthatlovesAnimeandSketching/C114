2d33506c6f262aa9e38fe6530a92d8f61bab08ba
AGirlthatlovesAnimeandSketching <80290196+AGirlthatlovesAnimeandSketching@users.noreply.github.com>
2022-04-06 17:57:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+clownnose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png")
+}
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+image(video,0,0,300,300)
+image(clownnose,0,0,30,30)
+}
+function take_snapshot(){
+    save("Filter.png");
+}
